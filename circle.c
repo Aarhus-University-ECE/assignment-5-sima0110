c4cd02d1e4f58c7e2baae17402d6e7544a3731eb
sima0110 <simajad.sj@gmail.com>
2022-10-01 17:19:24 +0200

    final

--- circle.c
+++ circle.c
@@ -1,19 +1,30 @@
 #include <malloc.h>
 #include "circle.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
+circle c[5];
 
 void fiveCircles(circle c[]) {
-/*post: returns an array with five circles - solution to 6.b*/
+for (int i = 0 ; i < 5; i++){
+   c[i].p.x = i; //sets value of i to c.p.x//
+   c[i].p.y = i; //sets value of i to c.p.y//
+   c[i].r = i; //sets value of i to c.r//
 }
-//svar
-int circleIsValid(const circle * c) {
-/*post: answer to exercise 6.c*/
-	return 0
+}
+
+bool circleIsValid(const circle * c) {
+if ((c-> r) > 0){ //checks the precondition (r>0)//
+	return true;
+ } 
+ else {
+	return false;
+}
 }
 
 void translate(circle* c, const point* p) {
-/*ansver to exercise 6.d
+
+c -> p.x = (c -> p.x) + (p -> x); 
+c -> p.y = (c -> p.y) + (p -> y);
 }
-
-
-
--- jollyJumper.c
+++ jollyJumper.c
@@ -1,11 +1,31 @@
 #include "jollyjumper.h"
 #include <stdbool.h>
 #include <malloc.h>
-
-
+#include <stdlib.h>
 
 int isJollyJumber(const int seq[], int size) {
-/*post: answer to exercise 7.a*/
-    return 1;
+bool diffs_found[size - 1];
+int diff; 
+
+for (int i = 0; i < size - 1; i++){
+diffs_found[i] = false;
+}
+
+for (int i = 1; i < size; i++){ //calculates the difference between the elements//
+    diff = abs(seq[i - 1] - seq[i]);
+    if (diff >= size || diff == 0){
+        return 0;
+    }
+    if (diffs_found[diff - 1] == false){
+        diffs_found[diff - 1] == true; 
+    }
+}; 
+
+for (int i = 0; i < size - 1; i++){
+    if (diffs_found[i] == false){
+    return 0;
+    }
+}
+ return 1;
 }
 
