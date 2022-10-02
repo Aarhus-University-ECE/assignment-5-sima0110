e0cc674d888f7913cb9fa087549e7826e1b018a2
sima0110 <simajad.sj@gmail.com>
2022-10-02 16:41:15 +0200

    final

--- main.c
+++ main.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include "circle.h"
+#include"jollyjumper.h"
+
+void printCircle(circle c) {
+    printf("p.x: %d, p.y: %d, r: %d\n", c.p.x, c.p.y, c.r);
+}
+
+int main(void) {
+    /*testing circles - make sure that you know how it works and what it is supposed to print out*/
+    circle c[5];
+    fiveCircles(c);
+    for (int i = 0; i < 5; i++) {
+        printCircle(c[i]);
+    }
+    point p;
+    p.x = 2;
+    p.y = 2;
+    translate(&c[1], &p);
+    printCircle(c[1]);
+    printf("isValid: %d", circleIsValid(&c[1]));
+
+    /*answer to exercise 7.b*/
+    int n; /*number of numbers to read*/
+
+    printf("Enter the number of elements: \n");
+    scanf("%d", &n);
+    assert(n > 1); //checking the precondition//
+
+    int *numbers = malloc(sizeof(int) * n); /*the numbers read*/
+
+    printf("Enter the elements:\n");
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &numbers[i]); //code for input//
+  }
+
+    if (isJollyJumber(numbers, n)) {
+        printf("it is a Jolly Jumper");}
+    else {
+        printf("not a Jolly Jumper");}
+    return 0;
 
